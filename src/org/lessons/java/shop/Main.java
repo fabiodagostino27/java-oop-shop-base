41b9fb9be1f8bbfb34e74f461f2cfa27f317e3e6
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-02 17:23:17 +0200

    ADD: getPrice and getPriceWithTaxes methods

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,11 +1,10 @@
 package org.lessons.java.shop;
 
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        Random rand = new Random();
+        Prodotto iphone16 = new Prodotto("iPhone 16", "L'ultimo smartphone rilasciato da Apple!", 999.99f, 22);
 
-        Prodotto iphone16 = new Prodotto(rand.nextInt(99), "iPhone 16", "L'ultimo smartphone rilasciato da Apple!", 999.99f, 22);
+        iphone16.getPrice();
+        System.out.printf("%.2f", iphone16.getPriceWithTaxes());
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,7 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
     public int code;
     public String name;
@@ -7,11 +9,23 @@ public class Prodotto {
     public float price;
     public int iva;
 
-    public Prodotto(int code, String name, String description, float price, int iva) {
-        this.code = code;
+    Random rand = new Random();
+
+    public Prodotto(String name, String description, float price, int iva) {
+        this.code = rand.nextInt(99);
         this.name = name;
         this.description = description;
         this.price = price;
         this.iva = iva;
+    }
+
+    public void getPrice() {
+        System.out.println(price);
+    }
+
+    public float getPriceWithTaxes() {    
+        float ivaValue = price * iva / 100;
+        float totalPrice = price + ivaValue;
+        return totalPrice;
     }
 }
