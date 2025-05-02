5128f5a5a3cab9618766a848f954d66db9096206
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-02 16:24:57 +0200

    ADD: fields to Prdotto class

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,9 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
-
+    public int code;
+    public String name;
+    public String description;
+    public float price;
+    public int iva;
 }
