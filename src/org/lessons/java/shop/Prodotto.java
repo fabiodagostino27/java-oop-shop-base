a10c4366445a55abadf37a87426f507a09f9049a
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-02 16:27:31 +0200

    ADD: constructor for Prodotto class

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -6,4 +6,12 @@ public class Prodotto {
     public String description;
     public float price;
     public int iva;
+
+    public Prodotto(int code, String name, String description, float price, int iva) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
 }
