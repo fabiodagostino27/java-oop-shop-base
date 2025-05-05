2d1e8bd63f43a95ea7ae7c6cf7c3e22d98644dbc
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-05 16:55:42 +0200

    ADD: implement getters and setters for Prodotto attributes; clean up Main class output

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -5,7 +5,9 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
         Prodotto iphone16 = new Prodotto("iPhone 16", "L'ultimo smartphone rilasciato da Apple!", new BigDecimal("999.99"), new BigDecimal("22"));
-        
+
+        System.out.println(iphone16.getCode() + " - " + iphone16.getName());
+        System.out.println(iphone16.getPrice());
         System.out.println(iphone16.getPriceWithTaxes());
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -13,7 +13,6 @@ public class Prodotto {
 
     Random rand = new Random();
 
-    
     public Prodotto(String name, String description, BigDecimal price, BigDecimal iva) {
         this();
         this.code = rand.nextInt(99);
@@ -22,20 +21,60 @@ public class Prodotto {
         this.price = price;
         this.iva = iva;
     }
-    
+
     public Prodotto() {
         System.out.println("Nuovo prodotto creato!");
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != null) {
+            this.name = name;
+        }
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        if (description != null) {
+            this.description = description;
+        }
     }
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public BigDecimal getPriceWithTaxes() {    
-        return price.add(price.multiply(iva).divide(new BigDecimal(100))).setScale(2, RoundingMode.HALF_UP);
+    public void setPrice(BigDecimal price) {
+        if (price != null) {
+            this.price = price;
+        }
     }
 
-    public void codePlusName() {
-        System.out.println(code + "-" + name);
+    public BigDecimal getIva() {
+        return iva;
+    }
+
+    public void setIva(BigDecimal iva) {
+        if (iva != null) {
+            this.iva = iva;
+        }
+    }
+
+    public BigDecimal getPriceWithTaxes() {
+        if (price != null && iva != null) {
+            return price.add(price.multiply(iva).divide(new BigDecimal(100))).setScale(2, RoundingMode.HALF_UP);
+        } else {
+            return null;
+        }    
     }
 }
